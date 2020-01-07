137eb7b06e028c29053cf23a1e296434104b8e95
Daniel Kipkemei <dann.professional@gmail.com>
2020-01-07 10:45:16 +0300

    feature(restaurant): view restaurant detail
    - view restaurant details
    - view restaurant schedule

---
+++ src/app/components/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantService } from '../../services/restaurant.service'
+import { environment } from '../../../environments/environment';
+
+@Component({
+  selector: 'app-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html',
+  styleUrls: ['./restaurant-detail.component.scss']
+})
+export class RestaurantDetailComponent implements OnInit {
+  slug:string
+  loading:boolean = false
+  restaurant:any
+  imgUrl:string
+  imagerandomizer:number
+  imageUrl = environment.unsplashUrl
+  status:string
+
+
+  constructor(private route: ActivatedRoute, private restaurantService:RestaurantService) { }
+
+  ngOnInit() {
+    this.loading = true
+    this.slug = this.route.snapshot.paramMap.get('slug')
+    this.getRestaurantRequest()
+
+    let imagerandomizer = Math.floor(Math.random() * 612)
+    this.imgUrl = this.imageUrl + "800x150/?sig=" + imagerandomizer
+
+  }
+
+  getRestaurantRequest() {
+    this.restaurantService.getRestaurant(this.slug).subscribe(restaurant =>{
+      this.restaurant = restaurant
+      this.loading = false
+
+      if (this.restaurant.open) {
+        this.status = "Open"
+      } else {
+        this.status = "Closed"
+      }
+    })
+  }
+
+  convertTime(time) {
+    let timeString = time;
+    let H = +timeString.substr(0, 2);
+    let h = H % 12 || 12;
+    let ampm = (H < 12 || H === 24) ? "am" : "pm";
+    timeString = h + timeString.substr(2, 3) + ampm;
+
+    return timeString
+  }
+
+  setClass() {
+    let classes = {
+      'open': this.restaurant.open
+    }
+    return classes
+  }
+
+}
